4fed548917a34a96b227c00e6cc9fbe87c7df18c
Edward <TuOfTimes@github.com>
2020-05-13 23:00:58 -0400

    updated giveaway verification, added logging

--- app.js
+++ app.js
@@ -1,53 +1,108 @@
 console.log("Twitter Giveaway Bot is starting...\n");
 
 var Twitter = require("twitter");
-var config = require("./config.js");
+var config = require("./config.json");
+var SortedSet = require("collections/sorted-set");
+var fs = require("fs");
 
-var T = new Twitter(config);
+var T = new Twitter(config.credentials);
 
 var params = {
-    q: "(giveaway OR win OR competition) (retweet OR RT)",
+    q: config.search_query,
     count: 100,
     result_type: "recent",
     lang: "en",
 };
 
-function getTweets(tweetHandler) {
+var tweets = new SortedSet(
+    [],
+    (a, b) => a.id_str == b.id_str,
+    (a, b) => {
+        if (a.id_str > b.id_str) {
+            return 1;
+        } else if (a.id_str < b.id_str) {
+            return -1;
+        }
+        return 0;
+    }
+);
+var following = []; // array of user_id strings, max 2000 following at a time
+
+function getTweets() {
     var count = 0;
     T.get("search/tweets", params)
         .then((data) => {
             console.log(`Query returned ${data.statuses.length} tweets`);
 
             for (let i = 0; i < data.statuses.length; i++) {
-                if (count == 20) {
-                    params.since_id = data.statuses[i].id_str;
-                    break;
-                }
-                // interact with tweet if it is actually a giveaway
                 if (
-                    data.statuses[i].in_reply_to_status_id_str == null &&
-                    !data.statuses[i].text.startsWith("RT @") &&
-                    !data.statuses[i].retweeted &&
-                    !data.statuses[i].text.includes("gay") &&
-                    !data.statuses[i].text.includes("nsfw") &&
-                    !data.statuses[i].text.includes("kpop") &&
-                    !data.statuses[i].text.includes("sugar daddy")
+                    data.statuses[i].retweeted_status &&
+                    isGiveaway(data.statuses[i].retweeted_status) &&
+                    !tweets.has(data.statuses[i].retweeted_status)
                 ) {
-                    // console.log(data.statuses[i]);
                     count++;
-                    tweetHandler(data.statuses[i]);
+                    tweets.push(data.statuses[i].retweeted_status);
+                    // recordData(
+                    //     "tweets.txt",
+                    //     data.statuses[i].retweeted_status.text + "\n---\n"
+                    // );
+                } else if (
+                    isGiveaway(data.statuses[i]) &&
+                    !tweets.has(data.statuses[i])
+                ) {
+                    count++;
+                    tweets.push(data.statuses[i]);
+                    // recordData("tweets.txt", data.statuses[i].text + "\n---\n");
                 }
             }
 
-            if (count != 20) {
-                params.since_id = data.search_metadata.max_id_str;
-            }
+            params.since_id = data.search_metadata.max_id_str;
 
-            console.log(`Entered ${count} giveaway(s)`);
+            console.log(tweets.length);
+            console.log(`Found ${count} giveaway(s)`);
+
+            recordData(
+                "getTweets.csv",
+                data.statuses.length + "," + count + "\n"
+            );
         })
         .catch((err) => {
             console.log(err);
         });
+}
+
+function isGiveaway(tweet) {
+    // filter tweets
+    if (
+        tweet.user.followers_count < config.min_follower_count ||
+        tweet.retweeted ||
+        tweet.is_quote_status ||
+        tweet.in_reply_to_status_id_str != null ||
+        tweet.retweeted_status != null
+    ) {
+        return false;
+    }
+
+    var tweetText = tweet.text.toLowerCase();
+    for (let j = 0; j < config.ignore_keywords.length; j++) {
+        if (tweetText.includes(config.ignore_keywords[j])) {
+            return false;
+        }
+    }
+
+    var userScreenName = tweet.user.screen_name.toLowerCase();
+    for (let j = 0; j < config.ignore_screen_names.length; j++) {
+        if (userScreenName == config.ignore_screen_names[j]) {
+            return false;
+        }
+    }
+
+    for (let j = 0; j < config.rewteet_keywords.length; j++) {
+        if (tweetText.includes(config.rewteet_keywords[j])) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function likeTweet(tweet) {
@@ -113,10 +168,22 @@ function enterGiveaway(tweet) {
     retweet(tweet);
 }
 
-getTweets(enterGiveaway);
+function recordData(file, data) {
+    fs.appendFile(file, data, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
 
-setInterval(() => {
+getTweets();
+var count = 0;
+var gettingTweets = setInterval(() => {
     console.log(`New since_id is ${params.since_id}`);
-    getTweets(enterGiveaway);
+    getTweets();
     console.log("\n");
-}, 1000 * 60 * 6);
+
+    if (++count == 2) {
+        clearInterval(gettingTweets);
+    }
+}, 1000 * 15);
